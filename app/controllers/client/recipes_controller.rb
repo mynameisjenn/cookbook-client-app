845012e772c3dda84e082b9342dc9bc023fdde41
mynameisjenn <jenniferlynnhatton@gmail.com>
2018-05-15 21:39:50 -0500

    added redirect and flash message

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -4,4 +4,60 @@ class Client::RecipesController < ApplicationController
     @recipes = response.body
     render 'index.html.erb'
   end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    client_params = {
+                      title: params[:title], 
+                      chef: params[:chef],
+                      ingredients: params[:ingredients],
+                      directions: params[:directions],
+                      prep_time: params[:prep_time],
+                      image_url: params[:image_url]
+                    }
+    response = Unirest.post("http://localhost:3000/api/recipes", parameters: client_params)
+
+    recipe = response.body
+    flash[:success] = "Successfully Added Recipe"
+    redirect_to "/client/recipes/#{recipe["id"]}"
+  end
+
+  def show
+    recipe_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/recipes/#{recipe_id}")
+    @recipe = response.body
+    render 'show.html.erb'
+  end
+
+  def edit
+    @recipe_id = params[:id]
+    response = Unirest.get("http://localhost:3000/api/recipes/#{@recipe_id}")
+    @recipe = response.body
+    render 'edit.html.erb'
+  end
+
+  def update
+    client_params = {
+                      title: params[:title], 
+                      chef: params[:chef],
+                      ingredients: params[:ingredients],
+                      directions: params[:directions],
+                      prep_time: params[:prep_time],
+                      image_url: params[:image_url]
+                    }
+
+    response = Unirest.patch("http://localhost:3000/api/recipes/#{params[:id]}",
+                              parameters: client_params)
+    recipe = response.body
+    redirect_to "/client/recipes/#{recipe["id"]}"
+  end
+
+  def destroy
+    recipe_id = params[:id]
+    response = Unirest.delete("http://localhost:3000/api/recipes/#{recipe_id}")
+    redirect_to "/"
+  end
 end
